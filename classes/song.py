a3b1e5b094c936c225a111aaf4f57ba3e9a1afc5
M <mrgmckenzie@gmail.com>
2023-04-14 13:56:19 +0100

    added a Song class and tests

---
+++ classes/song.py
@@ -0,0 +1,5 @@
+class Song:
+    def __init__(self, input_song_title, input_artist, input_song_length):
+        self.name = input_song_title
+        self.artist = input_artist
+        self.length = input_song_length
---
+++ tests/song_test.py
@@ -0,0 +1,16 @@
+import unittest
+from classes.song import Song
+
+class TestSong (unittest.TestCase):
+
+    def setUp(self):
+        self.example_song = Song("Let It Be", "The Beatles", 4)
+
+    def test_song_has_title(self):
+        self.assertEqual("Let It Be", self.example_song.name)
+
+    def test_song_has_artist(self):
+        self.assertEqual("The Beatles", self.example_song.artist)
+
+    def test_song_has_length(self):
+        self.assertEqual(4, self.example_song.length)
